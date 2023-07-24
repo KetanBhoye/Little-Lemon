856252e1b9cd1486dce728c48c9cf5f621773530
KetanBhoye <Imketanbhoye100@gmail.com>
2023-07-25 02:51:50 +0530

    Topbar Added

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.littlelemon
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.littlelemon.ui.theme.LowerPanel
 import com.example.littlelemon.ui.theme.UpperPanel
 
@@ -14,16 +16,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val context = LocalContext.current
-            Column() {
 
-                UpperPanel()
-                LowerPanel()
-
-            }
+    HomeScreen()
 
         }
     }
 
+  @Composable
+  fun HomeScreen(){
+      Scaffold(
+          topBar = {
+              TopAppBar()
+          }
+      ) {
+
+
+
+          Column {
+
+              UpperPanel()
+              LowerPanel()
+          }
+      }
+  }
 
 }
+
+
---
+++ app/src/main/java/com/example/littlelemon/TopAppBar.kt
@@ -0,0 +1,51 @@
+package com.example.littlelemon
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+    fun TopAppBar(){
+        Row(horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Image(
+                    painter = painterResource(id = R.drawable.menu),
+                    contentDescription = "Menu Icon",
+                    modifier = Modifier.size(30.dp)
+                )
+            }
+                Image(painter = painterResource(id = R.drawable.topbar_logo) ,
+                    contentDescription ="Top appbar logo",
+                    Modifier
+                        .fillMaxWidth(0.5F)
+                        .padding(7.dp)
+                        .scale(1.3F)
+                       )
+            IconButton(onClick = { /*TODO*/ }) {
+                Image(
+                    painter = painterResource(id = R.drawable.cart),
+                    contentDescription = "cart",
+                    modifier = Modifier.size(30.dp)
+                )
+
+            }
+        }
+    }
+
+
+@Preview(showBackground = true)
+@Composable
+fun TopAppBarPreview(){
+  TopAppBar()
+
+}
